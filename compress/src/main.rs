866c78f3fe92d9bfb2ff1a1e92577a6246d47f1a
Nasser Mohamed <nassermohamedit@gmail.com>
2024-05-05 14:07:34 +0100

    The basic-est compression tool ever

--- compress/src/main.rs
+++ compress/src/main.rs
@@ -1,16 +1,17 @@
-use std::fs;
 use flate2::{Compression, GzBuilder};
 use std::env;
+use std::error::Error;
+use std::fs;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::process::exit;
 
 /// The basic-est compression tool ever!
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut args: env::Args = env::args();
     if args.len() < 3 {
-        println!("Usage: [file_path] [compressed_file_path]");
+        eprintln!("Usage: [file_path] [compressed_file_path]");
         exit(1);
     }
     args.next();
@@ -18,19 +19,20 @@ fn main() {
     let compressed_file_path = args.next().unwrap();
     println!("arg1 = {file_path}, arg2 = {compressed_file_path}");
 
-    let mut input = BufReader::new(File::open(&file_path).unwrap());
-    let output = fs::File::create(compressed_file_path).unwrap();
+    let mut input = BufReader::new(File::open(&file_path)?);
+    let output = fs::File::create(compressed_file_path)?;
     let mut gz = GzBuilder::new()
         .filename(file_path)
         .write(output, Compression::default());
-    let mut buf = [0u8;100];
+    let mut buf = [0u8; 100];
 
-    loop  {
-        let n = input.read(&mut buf).unwrap();
+    loop {
+        let n = input.read(&mut buf)?;
         if n <= 0 {
             break;
         }
         println!("{n}");
         gz.write_all(&buf[..n]).expect("Failed compression. Oops!");
     }
+    Ok(())
 }
